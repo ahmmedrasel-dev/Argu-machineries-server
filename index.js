d76b13311f4595c4c7a465fb221eba95b94eaf9a
Ahmmed Rasel <rahmmed.info@gmail.com>
2022-05-21 22:27:35 +0300

    server setup and install dependenchy

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const cors = reqire('cors');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Argo Machineries Server Runnin from ${port}`)
+})
