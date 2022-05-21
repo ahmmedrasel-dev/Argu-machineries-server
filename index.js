ff242aff5e7242439db08b9edf0a15832ea7d95e
Ahmmed Rasel <rahmmed.info@gmail.com>
2022-05-21 22:36:59 +0300

    cors install

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const cors = reqire('cors');
+const cors = require('cors');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
