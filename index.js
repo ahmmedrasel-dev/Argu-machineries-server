cfc38e3dcc0915b7487e6a7615a3cbfaa33cfa56
Ahmmed Rasel <rahmmed.info@gmail.com>
2022-05-22 03:20:02 +0300

    create user post api

--- index.js
+++ index.js
@@ -4,11 +4,34 @@ const cors = require('cors');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
-
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.8uagw.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+  try {
+    await client.connect();
+    const userCollection = client.db('argo_machineries').collection('users');
+
+    // User Register Collection.
+    app.post('/user', async (req, res) => {
+      const user = req.body;
+      const result = await userCollection.insertOne(user);
+      res.send({ success: true, message: 'User Create Successfully!' })
+    })
+  }
+  finally {
+    // await client.close()/
+  }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
